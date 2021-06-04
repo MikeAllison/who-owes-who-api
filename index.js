78bd3277b4e73c5a22e0901d4d759c9dfec8df39
m <mikeallison79@gmail.com>
2021-06-04 17:15:41 -0400

    Finished GET transactions/active.  Some refactoring

--- index.js
+++ index.js
@@ -5,107 +5,106 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.GOOGLE_ACCOUNT_TYPE,
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_AUTH_URI,
-    token_uri: process.env.GOOGLE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL
-  })
-});
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      type: process.env.GOOGLE_ACCOUNT_TYPE,
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      auth_uri: process.env.GOOGLE_AUTH_URI,
+      token_uri: process.env.GOOGLE_TOKEN_URI,
+      auth_provider_x509_cert_url:
+        process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL
+    })
+  });
+} catch (err) {
+  console.log('There was a problem authenticating to Firestore');
+  console.log(err);
+}
+
+// Firestore
 const db = admin.firestore();
-
-// Collection - Cards
-//// Docs - Each Card
-////// Collection - Transactions
-//////// Doc - Each Transaction
-
 const cardsRef = db.collection('cards');
+const merchantsRef = db.collection('merchants');
 
 app.get('/cards', async (req, res) => {
+  const cards = [];
+
   try {
     const cardQuerySnapshot = await cardsRef.get();
-    const cards = [];
 
     cardQuerySnapshot.forEach(card => {
       cards.push({ id: card.id, cardholder: card.data().cardholder });
     });
-
-    res.status(200).json(cards);
   } catch (err) {
-    console.log(err);
-    req.status(500).send({ error: 'There was a problem with the request' });
+    res.status(500).send({ error: 'There was a problem with the request' });
   }
+
+  res.status(200).json(cards);
 });
 
-const merchantsRef = db.collection('merchants');
-
 app.get('/merchants', async (req, res) => {
+  const merchants = [];
+
   try {
     const merchantQuerySnapshot = await merchantsRef.get();
-    const merchants = [];
 
     merchantQuerySnapshot.forEach(merchant => {
       merchants.push({ id: merchant.id, name: merchant.data().name });
     });
-
-    res.status(200).json({ merchants });
   } catch (err) {
-    console.log(err);
-    req.status(500).send({ error: 'There was a problem with the request' });
+    res.status(500).send({ error: 'There was a problem with the request' });
   }
+
+  res.status(200).json({ merchants });
 });
 
 app.get('/transactions/active', async (req, res) => {
   const activeTransactions = [];
 
+  // First, add all cards to activeTransactions
   try {
     const cardQuerySnapshot = await cardsRef.get();
     cardQuerySnapshot.forEach(card => {
-      const cardTransactions = [];
-
-      // Get transactions
-      (async () => {
-        try {
-          const transactionsRef = cardsRef
-            .doc(card.id)
-            .collection('transactions');
-
-          const transactionsQuerySnapshot = await transactionsRef.get();
-
-          transactionsQuerySnapshot.forEach(transaction => {
-            cardTransactions.push({
-              id: transaction.id,
-              merchant: transaction.data().merchant,
-              amount: transaction.data().amount,
-              date: transaction.data().date.toDate(),
-              archived: transaction.data().archived
-            });
-          });
-        } catch (err) {
-          console.log(err);
-        }
-      })();
-
       activeTransactions.push({
         cardId: card.id,
         cardholder: card.data().cardholder,
-        transactions: cardTransactions
+        transactions: []
       });
     });
-
-    // TODO: Async/Await this
-    res.status(200).json(activeTransactions);
   } catch (err) {
-    console.log(err);
-    req.status(500).send({ error: 'There was a problem with the request' });
+    res.status(500).send({ error: 'There was a problem with the request' });
   }
+
+  // Second, add all transactions to each card in activeTransactions
+  try {
+    for (const card of activeTransactions) {
+      const transactionsRef = cardsRef
+        .doc(card.cardId)
+        .collection('transactions')
+        .where('archived', '==', false);
+
+      const transactionsQuerySnapshot = await transactionsRef.get();
+
+      transactionsQuerySnapshot.forEach(transaction => {
+        card.transactions.push({
+          id: transaction.id,
+          merchant: transaction.data().merchant,
+          amount: transaction.data().amount,
+          date: transaction.data().date.toDate(),
+          archived: transaction.data().archived
+        });
+      });
+    }
+  } catch (err) {
+    res.status(500).send({ error: 'There was a problem with the request' });
+  }
+
+  res.status(200).json(activeTransactions);
 });
 
 app.listen(process.env.PORT, () => {
